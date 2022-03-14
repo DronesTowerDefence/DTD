a93124b94d8743d038090e57f997edc074bae891
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2022-03-14 17:07:17 +0100

    Drone-Klasse hinzugefügt (WIP)

---
+++ 01. Eingerichtet/Drone.cpp
@@ -0,0 +1,24 @@
+
+#include "Drone.h"
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+Drone::Drone(int typ)
+{
+
+	droneTexture = Texture();
+	droneTexture.loadFromFile("image/drone2_40x40.png");
+	drone = Sprite();
+	drone.setTexture(droneTexture);
+}
+
+void Drone::setPosition(Vector2f position)
+{
+	this->position = position;
+
+}
+
+Vector2f Drone::getPosition()
+{
+	return Vector2f();
+}
