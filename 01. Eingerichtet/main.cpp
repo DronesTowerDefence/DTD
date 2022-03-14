c4a8a1bffcbd490f86954208407f284e66cb331a
Jonas Eberhardt <jonas.eberhardt@schueler.max-eyth-schule.de>
2022-03-14 13:29:16 +0100

    Tower-Klasse hinzugefügt (ist aber noch nicht fertig)

--- 01. Eingerichtet/main.cpp
+++ 01. Eingerichtet/main.cpp
@@ -9,10 +9,10 @@ using namespace sf; // statt std, hat zur Folge, dass bei String etc. std:: vor 
 int main() {
 	RenderWindow window(VideoMode(1920, 991), "DronesTowerDefence");
 
-	RectangleShape test = RectangleShape(Vector2f(40, 40)); 
+	/*RectangleShape test = RectangleShape(Vector2f(40, 40));
 	test.setFillColor(Color::Magenta);
 
-	Map* map1 = new Map(); 
+	Map* map1 = new Map();
 
 	Vector2f position = map1->getStart();
 	test.setPosition(position);
@@ -33,8 +33,10 @@ int main() {
 	Texture* restart = new Texture();
 	restart->loadFromFile("image/Map1.png");
 	RectangleShape background  = RectangleShape(Vector2f(1920, 1080));
-	background.setTexture(restart);
-
+	background.setTexture(restart);*/
+	RectangleShape test = RectangleShape(Vector2f(500, 991));
+	test.setFillColor(Color::Red);
+	test.setPosition(Vector2f(1720, 0));
 	while (window.isOpen())
 	{
 		Event event;
@@ -43,14 +45,20 @@ int main() {
 			if (event.type == Event::Closed)
 				window.close();
 		}
-		Vector2f pos = test.getPosition();
+		/*Vector2f pos = test.getPosition();
 		koadinaten.setString(std::to_string(pos.x) + "\n" + std::to_string(pos.y));
 		map1->chekChanceRichtung(&test, &x_richtung, &	y_richtung, v);
 		window.clear();
 		window.draw(background);
 		window.draw(koadinaten);
 		window.draw(test);
+		window.display();*/
+		window.draw(test);
 		window.display();
-	}
+		}
+
+
+	
+	
 	return 0;
 }
