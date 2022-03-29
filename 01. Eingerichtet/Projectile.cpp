3e727b59dbfe90da714f29a212ca9168c3dae2d7
Amon Sarfo <amonjoels@gmail.com>
2022-03-29 15:28:15 +0200

    Projektil vielleicht fertig

--- 01. Eingerichtet/Projectile.cpp
+++ 01. Eingerichtet/Projectile.cpp
@@ -14,8 +14,8 @@ Vector3f Projectile::targeting(Drone* _Drone)
 {
 	float buffer = 10;
 	for (auto i : tower->getCoverableArea()) {
-			if (_Drone->getNextPosition(i.z*speed).x - i.x<5 && _Drone->getNextPosition(i.z*speed).x - i.x> -5) {
-				if (_Drone->getNextPosition(i.z * speed).y - i.y * speed<5 && _Drone->getNextPosition(i.z * speed).y - i.y > -5) {
+			if (_Drone->getNextPosition(i.z/speed).x - i.x<5 && _Drone->getNextPosition(i.z/speed).x - i.x> -5) {
+				if (_Drone->getNextPosition(i.z/speed).y - i.y<5 && _Drone->getNextPosition(i.z/speed).y - i.y > -5) {
 					return i;
 				}
 			}
@@ -24,6 +24,6 @@ Vector3f Projectile::targeting(Drone* _Drone)
 
 void Projectile::moveProjectile()
 {
-
+	
 }
 
