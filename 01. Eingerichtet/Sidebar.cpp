9136e5fc121945fdc0687d16edac9040613dcd0c
Jonas Eberhardt <jonas.eberhardt@schueler.max-eyth-schule.de>
2022-03-29 08:26:54 +0200

    Sidebar: Jetzt werden die Tower angezeigt und nicht mehr die Drohnen

--- 01. Eingerichtet/Sidebar.cpp
+++ 01. Eingerichtet/Sidebar.cpp
@@ -42,10 +42,10 @@ void Sidebar::draw(sf::RenderWindow* window)
 
 Sidebar::Sidebar()
 {
-	towers[0] = new BuyTower(100, Vector2f(50, 50), Vector2f(1775, 15), "img/drone0_40x40.png");
-	towers[1] = new BuyTower(200, Vector2f(50, 50), Vector2f(1850, 15), "img/drone0_40x40.png");
-	towers[2] = new BuyTower(300, Vector2f(50, 50), Vector2f(1775, 100), "img/drone0_40x40.png");
-	towers[3] = new BuyTower(400, Vector2f(50, 50), Vector2f(1850, 100), "img/drone0_40x40.png");
+	towers[0] = new BuyTower(100, Vector2f(50, 50), Vector2f(1775, 15), "img/tower0_50x50.png");
+	towers[1] = new BuyTower(200, Vector2f(50, 50), Vector2f(1850, 15), "img/tower1_50x50.png");
+	towers[2] = new BuyTower(300, Vector2f(50, 50), Vector2f(1775, 100), "img/tower2_50x50.png");
+	towers[3] = new BuyTower(400, Vector2f(50, 50), Vector2f(1850, 100), "img/tower3_50x50.png");
 
 	backround = RectangleShape();
 	backround.setFillColor(Color::Blue);
