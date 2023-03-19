36030a6964c958812d7aaa8ab85e5c1bfd59504a
Jonas Eberhardt <jonas.eberhardt@schueler.max-eyth-schule.de>
2023-03-19 18:38:39 +0100

    #78

--- 01. Eingerichtet/Service.cpp
+++ 01. Eingerichtet/Service.cpp
@@ -33,7 +33,65 @@ Vector2f Service::getObjectPosition(Vector2f objectposition)
 }
 float Service::stringToFloat(std::string str)
 {
-	return float(stringToInt(str));
+	float returnValue = 0.f, returnValue2 = 0.0f;
+	bool isComma = false;
+	int decimalCount = 0;
+	char tmp = '0';
+
+	// Untersucht ob der String gültig ist
+	for (int i = 0, a = 0, b = 0; i < str.size(); i++)
+	{
+		// Wenn etwas anderes als eine Zahl, Komma oder Punkt im String ist
+		if ((str[i] >= 48 && str[i] <= 57) || str[i] == 46 || str[i] == 44)
+			return 0.0f;
+
+		a = 0;
+		a = str.find(',', a);
+
+		if (a != std::string::npos)
+			b++;
+
+		a = 0;
+		a = str.find('.', a);
+
+		if (a != std::string::npos)
+			b++;
+
+		// Wenn mehr als ein Komma/Punkt enthalten ist
+		if (b > 1)
+			return 0.0f;
+	}
+
+	// Berechnet returnValue und returnValue2 vom string
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == ',' || str[i] == '.')
+		{
+			isComma = true;
+		}
+		else
+		{
+			if (isComma)
+			{
+				returnValue2 *= 10;
+				returnValue2 += (str[i] - 48);
+				decimalCount++;
+			}
+			else
+			{
+				returnValue *= 10;
+				returnValue += (str[i] - 48);
+			}
+		}
+	}
+
+	// Macht aus returnValue2 eine Nach-Komma-Zahl
+	for (int i = 0; i < decimalCount; i++)
+	{
+		returnValue2 *= 0.1;
+	}
+
+	return returnValue + returnValue2;
 }
 int Service::stringToInt(std::string str)
 {
